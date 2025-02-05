5b11a7d3417d5c7b73538303f9a82fbb89accf87
Vishal Raj <vishal.raj@elastic.co>
2025-02-05 14:44:48 +0530

    Revert pebble v2 bump up

--- processor/lsmintervalprocessor/processor.go
+++ processor/lsmintervalprocessor/processor.go
@@ -25,8 +25,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/cockroachdb/pebble/v2"
-	"github.com/cockroachdb/pebble/v2/vfs"
+	"github.com/cockroachdb/pebble"
+	"github.com/cockroachdb/pebble/vfs"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottldatapoint"
 	"go.opentelemetry.io/collector/client"
 	"go.opentelemetry.io/collector/component"
